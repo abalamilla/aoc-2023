560cffd74b2decf2ee25f49390c3f6fd18b98fb3
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-12-04 16:23:08 -0600

    aoc 2023 day 1 - part 2: apparently something is wrong 🤔
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- day-1-trebuchet/trebuchet.pl
+++ day-1-trebuchet/trebuchet.pl
@@ -1,22 +1,47 @@
+numbers(one, '1').
+numbers(two, '2').
+numbers(three, '3').
+numbers(four, '4').
+numbers(five, '5').
+numbers(six, '6').
+numbers(seven, '7').
+numbers(eight, '8').
+numbers(nine, '9').
+
 first_digit([H|_], Digit) :-
 	char_type(H, digit),
 	Digit = H.
 first_digit([_|T], Digit) :-
 	first_digit(T, Digit).
 
-lookup_digit(String, Digit) :-
-	string_chars(String, Chars),
-	first_digit(Chars, Digit).
+verify_fact(String, Fact, Remaining) :-
+	append(FactAtom, Remaining, String),
+	atom_string(FactString, FactAtom),
+	numbers(FactString, Fact).
+
+find_spelled_numbers([], []).
+find_spelled_numbers([C|Rest], [NewC|NewRest]) :-
+	verify_fact([C|Rest], Fact, Remaining),
+	!,
+	find_spelled_numbers(Remaining, NewRest),
+	NewC = Fact.
+find_spelled_numbers([C|Rest], [C|NewRest]) :-
+	find_spelled_numbers(Rest, NewRest).
+
+replace_spelled_numbers(String, Replaced) :-
+	atom_chars(String, Chars),
+	find_spelled_numbers(Chars, Replaced).
 
 process_lines([First|Rest], Accum, Sum) :-
-	string_chars(First, Chars),
-	first_digit(Chars, FirstDigit),
-	reverse(Chars, Reversed),
+	replace_spelled_numbers(First, Replaced),
+	first_digit(Replaced, FirstDigit),
+	reverse(Replaced, Reversed),
 	first_digit(Reversed, LastDigit),
 	concat_atom([FirstDigit, LastDigit], Concat),
 	atom_number(Concat, Digit),
 	process_lines(Rest, Digit + Accum, Sum).
-process_lines([], Accum, Sum) :- Sum is Accum, !.
+process_lines([], Accum, Sum) :- 
+	Sum is Accum.
 
 sum_calibration(Input, Sum) :-
 	process_lines(Input, 0, Sum).
@@ -36,6 +61,5 @@ load_file(Filename, Lines) :-
 
 main :-
 	load_file("input.txt", Input),
-	write(Input), nl,
 	sum_calibration(Input, Sum),
 	write(Sum), nl.
