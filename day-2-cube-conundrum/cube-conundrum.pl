868830650a6c883fc255b858983f3951ecf18f0b
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-12-06 08:14:40 -0600

    aoc 2023 day 2 - part 1: refactoring
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- day-2-cube-conundrum/cube-conundrum.pl
+++ day-2-cube-conundrum/cube-conundrum.pl
@@ -1,42 +1,54 @@
-score("blue", 14).
-score("red", 12).
-score("green", 13).
+max_score("blue", 14).
+max_score("green", 13).
+max_score("red", 12).
 
-validate_score(Color, Score) :-
-    score(Color, Max),
-    Score =< Max.
+color(blue).
+color(green).
+color(red).
 
-validate_scores([]).
-validate_scores([Score|Rest]) :-
-    split_string(Score, " ", " ", [ScoreValue, Color]),
-    atom_number(ScoreValue, ScoreNumber),
-    validate_score(Color, ScoreNumber),
-    validate_scores(Rest).
+id(N) :- between(1, 100, N).
 
-validate_subgames([]) :- !.
-validate_subgames([Subgame|Rest]) :-
-    split_string(Subgame, ",", "", Scores),
-    validate_scores(Scores),
-    validate_subgames(Rest).
+score([Score, Color]) :-
+    atom_number(Score, ScoreNum),
+    max_score(Color, Max),
+    between(0, Max, ScoreNum).
 
-validate_game(Game, Id) :-
-    re_matchsub("Game (?<id>\\d+): (?<games>.*)", Game, Matches, []),
-    split_string(Matches.games, ";", "", Subgames),
-    validate_subgames(Subgames),
-    atom_number(Matches.id, Id).
+valid_subset([]).
+valid_subset([Set|Rest]) :-
+    score(Set),
+    valid_subset(Rest).
 
-sum_valid_games(Input, Sum) :-
-    sum_valid_games(Input, 0, Sum).
+valid_set([]).
+valid_set([Subset|Rest]) :-
+    valid_subset(Subset),
+    valid_set(Rest).
 
-sum_valid_games([], Sum, Sum).
-sum_valid_games([Game|Rest], Accum, Sum) :-
-    validate_game(Game, Id),
-    NewAccum is Accum + Id,
-    sum_valid_games(Rest, NewAccum, Sum).
-sum_valid_games([Game|Rest], Accum, Sum) :-
-    \+ validate_game(Game, _),
-    sum_valid_games(Rest, Accum, Sum).
-    
+game(Id, Sets) :-
+    id(Id),
+    valid_set(Sets).
+
+my_split_string(Sep, String, Substrings) :-
+    split_string(String, Sep, " ", Substrings).
+
+parse_line("", _, []).
+parse_line(Line, Id, Sets) :-
+    split_string(Line, ":", "", [GameId, Rest]),
+    split_string(GameId, " ", "", [_, IdStr]),
+    atom_number(IdStr, Id),
+    split_string(Rest, ";", "", Substrings),
+    maplist(my_split_string(","), Substrings, ColorSets),
+    maplist(maplist(my_split_string(" ")), ColorSets, Sets).
+
+sum_valid_games([], 0).
+sum_valid_games([Line|Rest], Sum) :-
+    parse_line(Line, Id, Sets),
+    game(Id, Sets),
+    sum_valid_games(Rest, RestSum),
+    Sum is RestSum + Id.
+sum_valid_games([Line|Rest], Sum) :-
+    parse_line(Line, Id, Sets),
+    \+ game(Id, Sets),
+    sum_valid_games(Rest, Sum).
 
 %! test.
 
@@ -54,4 +66,7 @@ load_file(Filename, Lines) :-
 main :-
 	load_file("input.txt", Input),
 	sum_valid_games(Input, Sum),
+    %sum_valid_games_v2(Input, SumV2),
 	write(Sum), nl.
+    %write(SumV2), nl.
+
